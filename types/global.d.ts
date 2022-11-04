7591e1819fd4b8f4b87cef318c7365786a24738f
pqj <3405008677@qq.com>
2022-11-04 17:54:58 +0800

    11-4

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,5 +1,25 @@
-// vue
+import type {
+  ComponentRenderProxy,
+  VNode,
+  VNodeChild,
+  ComponentPublicInstance,
+  FunctionalComponent,
+  PropType as VuePropType,
+} from "vue";
 
+// vue
+// export {};
 declare global {
   declare type Recordable<T = any> = Record<string, T>;
+
+  // env list o(╥﹏╥)o
+  declare interface ViteEnv {
+    VITE_PORT: number;
+  }
+}
+
+declare module "vue" {
+  export type JSXComponent<Props = any> =
+    | { new (): ComponentPublicInstance<Props> }
+    | FunctionalComponent<Props>;
 }
