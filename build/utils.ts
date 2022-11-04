3e04de8b71bffb717d89236b069eff1dec870852
pqj <3405008677@qq.com>
2022-11-04 17:11:55 +0800

    11-4

--- build/utils.ts
+++ build/utils.ts
@@ -1,6 +1,7 @@
 export function wrapperEnv(envConf: Recordable): ViteEnv {
   const ret: any = {};
   for (const envName of Object.keys(envConf)) {
+    let realName = envConf[envName].replace(/\\n/g, "\n");
     // 如果是boolean类型就转换
     realName = realName === "true" ? true : realName === "false" ? false : realName;
   }
