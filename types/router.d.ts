26cd44d2fdb58de0cdfc9bc0c5dc50325f0a9584
pqj <3405008677@qq.com>
2022-11-07 17:47:44 +0800

    11-7

---
+++ types/router.d.ts
@@ -0,0 +1,28 @@
+export {}
+
+declare module 'vue-router' {
+  interface RouteMeta {
+    title?: string
+    elIcon?: string
+    needLogin?: boolean
+    keepAlive?: boolean
+  }
+  interface RouteRule extends Record<string | number | symbol, unknown> {
+    path: string
+    name: string
+    redirect?: string
+    meta?: RouteMeta
+    component?: any
+    children?: RouteRule
+  }
+  interface MyRouter {
+    menuId: string
+    roleId: number
+    parentId: string
+    name: string
+    type: number
+    icon: string
+    path: string
+    sysMenus: Array<MyRouter>
+  }
+}
